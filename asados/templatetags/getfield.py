a91055d5e8570e9566029d93d885ea95e07c8d29
Enzo Cioppettini <tenstrings5050@gmail.com>
2017-10-25 02:09:59 -0300

    Me había olvidado de agregar los archivos

---
+++ asados/templatetags/getfield.py
@@ -0,0 +1,7 @@
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def getfield(an_object,an_attribute):
+    return getattr(an_object,an_attribute)
---
+++ asados/templatetags/split_with.py
@@ -0,0 +1,7 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def split_with(a_string, a_separator):
+    return a_string.split(a_separator)
