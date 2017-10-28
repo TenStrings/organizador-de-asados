ad9e9e121860f2bf166d5e3780e814ebe454a5bb
Enzo Cioppettini <tenstrings5050@gmail.com>
2017-10-28 02:26:43 -0300

    Cambios estilísticos para adherir a PEP8

--- asados/views.py
+++ asados/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template.loader import get_template
 from datetime import datetime
-from .models import Asado,User,Invitation,Assignment,AssignmentValidationError
-from .forms import AddAsadoForm,AddUserForm,AddAssignmentForm
+from .models import (
+    Asado,
+    User,
+    Invitation,
+    Assignment,
+    AssignmentValidationError
+)
+from .forms import AddAsadoForm, AddUserForm, AddAssignmentForm
 from django.core.exceptions import ValidationError
 from django.urls import reverse
+
 
 def homepage(request):
     template = get_template('homepage.html')
@@ -19,15 +26,16 @@ def homepage(request):
     asados = Asado.objects.all()
     return HttpResponse(
         template.render(
-            request = request,
-            context = {
-                'lista_de_asados' : asados,
-                'form' : form
+            request=request,
+            context={
+                'lista_de_asados': asados,
+                'form': form
             }
         )
     )
 
-def asado_id(request,a_valid_id):
+
+def asado_id(request, a_valid_id):
     template = get_template('asado-de.html')
     asado = Asado.objects.get(id=a_valid_id)
     invites = asado.attendee.all()
@@ -44,38 +52,40 @@ def asado_id(request,a_valid_id):
 
     items_to_buy = asado.shop_list.all()
     estimated_by_items = sum(
-        [ item.estimated_cost() for item in items_to_buy ]
+        [item.estimated_cost() for item in items_to_buy]
     )
 
     estimated_cost = asado.estimated_cost
 
     return HttpResponse(
                 template.render(
-                    request = request,
-                    context = {
-                        'lista_de_invitados' : invites,
-                        'items_a_comprar' : items_to_buy,
-                        'form' : form,
-                        'estimated_cost' : estimated_cost
+                    request=request,
+                    context={
+                        'lista_de_invitados': invites,
+                        'items_a_comprar': items_to_buy,
+                        'form': form,
+                        'estimated_cost': estimated_cost
                     }
                 )
             )
 
-def personal_page(request,username):
+
+def personal_page(request, username):
     user = User.objects.get(name=username)
     template = get_template('personal-page.html')
 
     invites = Invitation.objects.filter(invite=user)
-    asados = [invite.asado for invite in invites ]
+    asados = [invite.asado for invite in invites]
     return HttpResponse(
         template.render(
             request=request,
-            context= {
-                'username' : user.name,
-                'asados' : asados,
+            context={
+                'username': user.name,
+                'asados': asados,
             }
         )
     )
+
 
 def add_user(request):
     template = get_template('user-creation.html')
@@ -87,12 +97,13 @@ def add_user(request):
         form = AddUserForm()
 
     return HttpResponse(template.render(
-        request = request,
-        context = {
-            'form' : form,
-            'users' : User.objects.all()
+        request=request,
+        context={
+            'form': form,
+            'users': User.objects.all()
         }
     ))
+
 
 def select_user(request):
     template = get_template('user-select.html')
@@ -102,11 +113,14 @@ def select_user(request):
             return HttpResponseRedirect('/'+request.GET['username']+'/')
 
     return HttpResponse(template.render(
-        request = request,
-        context = { 'users': User.objects.all() }
+        request=request,
+        context={
+            'users': User.objects.all()
+        }
     ))
 
-def pending_list(request,asado_id,username):
+
+def pending_list(request, asado_id, username):
     template = get_template("shopping-list.html")
     asado = Asado.objects.get(id=asado_id)
     user = User.objects.get(name=username)
@@ -124,7 +138,8 @@ def pending_list(request,asado_id,username):
         )
     )
 
-def commit_assignment(request,assignment_id):
+
+def commit_assignment(request, assignment_id):
     template = get_template("commit-assignment.html")
     assignment = Assignment.objects.get(id=assignment_id)
     error_message = ''
@@ -141,8 +156,8 @@ def commit_assignment(request,assignment_id):
                 reverse(
                     'encargos',
                     args=[
-                            assignment.asado.id,
-                            assignment.designated_user.name
+                        assignment.asado.id,
+                        assignment.designated_user.name
                     ]
                 )
             )
@@ -153,8 +168,8 @@ def commit_assignment(request,assignment_id):
         template.render(
             request=request,
             context={
-            'assignment': assignment,
-            'error_message' : error_message
+                'assignment': assignment,
+                'error_message': error_message
             }
         )
     )
